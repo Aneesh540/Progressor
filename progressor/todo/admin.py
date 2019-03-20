adc27ec434628e6f2aadff4d1d6ceb39c358feec
Aneesh540 <aneeshjain203@gmail.com>
2019-03-21 02:34:42 +0530

    Blueprint of app ready

---
+++ progressor/todo/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Directory,Todoentry,Details
+# Register your models here.
+admin.site.register(Directory)
+admin.site.register(Todoentry)
+admin.site.register(Details)
---
+++ progressor/todo/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils import timezone
+
+class Directory(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    necessary = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{0} | {1}".format(self.name,self.id)
+
+
+
+class Todoentry(models.Model):
+    # null=True for allowing null in database, blank=True to allow blank in form
+    basedir = models.ForeignKey(Directory,on_delete=models.CASCADE)
+
+    entry = models.CharField(max_length=100,blank=False,null=False)
+
+    create_date = models.DateTimeField(default=timezone.now)
+    due_date = models.DateField(blank=True, null=True)
+
+    important = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{0} | {1}".format(self.entry, self.basedir.name)
+
+
+class Details(models.Model):
+
+    which_entry = models.ForeignKey(Todoentry,on_delete=models.CASCADE)
+    subtask = models.CharField(max_length=100,blank=False,null=False)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.subtask
